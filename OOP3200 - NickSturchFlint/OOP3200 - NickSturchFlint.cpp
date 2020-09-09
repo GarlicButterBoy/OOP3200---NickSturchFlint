75bd57a7003b69ab073e3608aacbd82189066663
Nick Sturch-Flint <nick.rulez@live.com>
2020-09-09 11:37:36 -0400

    Add a new function to welcome any users to OOP3200

--- OOP3200 - NickSturchFlint/OOP3200 - NickSturchFlint.cpp
+++ OOP3200 - NickSturchFlint/OOP3200 - NickSturchFlint.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+static void myFunction()
+{
+    std::cout << "Welcome to OOP 3200!!" << std::endl;
+}
+
 int main()
 {
+
+
+    //hello world
     std::cout << "Hello World!\n";
+    myFunction();
 
 }
